0b1969e2edbaa161f4a58f20c27a4fa6eaab776f
zwpdbh <hyperion_z@outlook.com>
2017-07-28 21:29:15 +1200

    >

--- lab2.py
+++ lab2.py
@@ -1,50 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import mylib
+from FuzzyCMeans import CMeans
 
+d1 = np.random.randn(100)+2
+d2 = np.random.randn(100)+8
+d3 = np.random.randn(100)+14
 
+data=np.concatenate((d1,d2,d3),axis=0)
+data=np.random.permutation(data)
 
-d1 = np.random.randn(100) + 2
-d2 = np.random.randn(100) + 8
-
-# plt.plot(d1, 'g+')
-# plt.plot(d2, 'bx')
-
-data = np.concatenate((d1, d2), axis=0)
-data = np.random.permutation(data)
-
-# data[80] = 200
-
-nrow= len(data)
-
-# kMeans cluster
-# clusteringResult = mylib.kMeans(data=data, num_of_clusters=2, num_of_loop=20)
-#
-# centroid1 = clusteringResult[0][0]
-# centroid2 = clusteringResult[0][1]
-#
-# cluster1 = clusteringResult[1][0]
-# cluster2 = clusteringResult[1][1]
-#
-# clusterData1 = []
-# clusterData2 = []
-#
-# for i in cluster1:
-#     clusterData1.append(data[i])
-#
-# for i in cluster2:
-#     clusterData1.append(data[i])
-
-# plt.plot(d1, 'g+')
-# plt.plot(d2, 'bx')
-# plt.plot(nrow/4, centroid1,'r^')
-# plt.plot(nrow/4, centroid2,'r^')
-# plt.show()
-
-# cMeans cluster
-cMean = mylib.CMeans(dataSet=data, numberOfClusters=2, numOfLoop=20)
+cMean = CMeans(dataSet=data, numberOfClusters=3, iterationCount=20)
 centroids = cMean.getCentroids()
-print centroids
+plt.plot(d1,'g+')
+plt.plot(d2,'bx')
+plt.plot(d3,'g+')
 
-kMeans = mylib.KMeans(dataSet=data, numOfClusters=2, numOfLoop=20)
-print kMeans.getCentroids()
+for centroid in centroids:
+    plt.plot(len(cMean.dataSet) / 4, centroid, 'r^')
+plt.show()
