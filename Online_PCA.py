d6c55612ee73477c0ec17e8db9cee6284e50dc9c
zwpdbh <hyperion_z@outlook.com>
2017-08-31 21:55:35 +1200

    > ready

--- Online_PCA.py
+++ Online_PCA.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pylab as plt
 import numpy.linalg as LA
-import matplotlib.pyplot as plt
 
 '''
 The powerPCA method given about carries out PCA ina batch mode. It needs to gather all data
@@ -33,34 +32,26 @@ class OnlinePCA:
         self.n = self.X.shape[0]
         self.initialR = self.R.copy()
 
-    def getCovarianceMatrix(self):
-        return self.R
-
-    def updateCov(self, x, gamma=None):
-        self.n += 1
-        x = np.matrix(x)
-        if gamma==None:
-            gamma = (1.0 / self.n)
-        # self.m = self.m + gamma * (x - self.m)
-        self.R = self.R + gamma * (x.T * x - self.R)
-
-    def converged(self, w, w1, thres=1e-10):
+    @staticmethod
+    def converged(w, w1, thres=1e-10):
         convergent = False
         corr = w.T * w1
         if abs(corr) > 1 - thres:
             convergent = True
         return convergent
 
-    def power_iteration(self, R, w):
+    @staticmethod
+    def power_iterations(R, w):
         while 1:
             w1 = w.copy()
             w = R * w
             w = w / LA.norm(w)
-            if self.converged(w, w1):
+            if OnlinePCA.converged(w, w1):
                 break
         return w
 
-    def plot_embedding(self, X, labels, title=None):
+    @staticmethod
+    def plot_embedding(X, labels, title=None):
         y = labels.copy()
         x_min, x_max = np.min(X, 0), np.max(X, 0)
         X1 = (X - x_min) / (x_max - x_min)
@@ -75,19 +66,61 @@ class OnlinePCA:
         if title is not None:
             plt.title(title)
 
+    @staticmethod
+    def powerPCA(data, n_pcs):
+        nr, dim = data.shape
+        X = np.matrix(data)
+        m = np.mean(data, axis=0)
+        R = (X - m).T * (X - m) / (nr - 1.0)
+        R0 = R.copy()
+
+        # initialize
+        w = np.matrix(np.random.rand(dim)).T
+        w /= LA.norm(w)
+        # iterate for 1st eigenvector
+        w = OnlinePCA.power_iterations(R, w)
+        # first eigenvector as first column of W
+        W = w
+        # iterate for other eigenvectors
+        for i in range(1, n_pcs):
+            # deflate R
+            R -= w * w.T * R
+            # initialize and Power iter
+            w = np.matrix(np.random.rand(dim)).T
+            w /= LA.norm(w)
+            w = OnlinePCA.power_iterations(R, w)
+            # attach the new eigenvector to W as a new column
+            W = np.c_[W, w]
+
+        # get eigenvalues and save them in the ev array
+        y = R0 * W
+        ev = LA.norm(y, axis=0) / LA.norm(W, axis=0)
+        return W, ev
+
+    def getCovarianceMatrix(self):
+        return self.R
+
+    def updateCov(self, x, gamma=None):
+        self.n += 1
+        x = np.matrix(x)
+        if gamma==None:
+            gamma = (1.0 / self.n)
+        # self.m = self.m + gamma * (x - self.m)
+        self.R = self.R + gamma * (x.T * x - self.R)
+
     # compute the eigenvectors and eigenvalues based on the current covariance matrix stored as self.R
-    def computePCA(self, n_pcs):
+    def onlinePowerPCA(self, n_pcs):
         R0 = self.R.copy()
         w = np.matrix(np.random.rand(self.dim)).T
         w = w / LA.norm(w)
-        w = self.power_iteration(self.R, w)
+        w = self.power_iterations(self.R, w)
 
         W = w
         for i in range(1, n_pcs):
             self.R = self.R - w * w.T * self.R
             w = np.matrix(np.random.rand(self.dim)).T
             w = w / LA.norm(w)
-            w = self.power_iteration(self.R, w)
+            w = self.power_iterations(self.R, w)
             W = np.c_[W, w]
 
         y = R0 * W
@@ -97,7 +130,7 @@ class OnlinePCA:
     # w0 is the batch mode eigenvector
     def compareWithCurrentEigenvector(self, w0):
         # compute the first eigenvector, based on the current updated covariance matrix
-        e1, ev = self.computePCA(n_pcs=1)
+        e1, ev = self.onlinePowerPCA(n_pcs=1)
 
         e1 = np.squeeze(np.asarray(e1))
         w0 = np.squeeze(np.asarray(w0))
@@ -106,6 +139,5 @@ class OnlinePCA:
         s = 0
         for i in range(w0.shape[0]):
             s += (e1[i] - mean_e1) * (w0[i] - mean_w0)
-            # print (e1[i] - mean_e1) * (w0[i] - mean_w0)
         return s
 
--- asgn1.py
+++ asgn1.py
@@ -6,86 +6,32 @@ import matplotlib.pyplot as plt
 
 
 ##############
-# Power method
-def power_iterations(R, w):
-    while 1:
-        w1 = w.copy()
-        w = R * w
-        w /= LA.norm(w)
-        if converged(w, w1):
-            break
-    return w
 
 
-# test whether two normalized vectors are the same (or deviation within a threshold)
-def converged(w, w1, thres=1e-10):
-    converged = False
-    corr = w.T * w1
-    # print corr
-    if abs(corr) > 1 - thres:
-        converged = True
-    return converged
-
-
-def powerPCA(data, n_pcs):
-    nr, dim = data.shape
-    X = np.matrix(data)
-    m = np.mean(data, axis=0)
-    R = (X - m).T * (X - m) / (nr - 1.0)
-    R0 = R.copy()
-
-    # initialize
-    w = np.matrix(np.random.rand(dim)).T
-    w /= LA.norm(w)
-    # iterate for 1st eigenvector
-    w = power_iterations(R, w)
-    # first eigenvector as first column of W
-    W = w
-    # iterate for other eigenvectors
-    for i in range(1, n_pcs):
-        # deflate R
-        R -= w * w.T * R
-        # initialize and Power iter
-        w = np.matrix(np.random.rand(dim)).T
-        w /= LA.norm(w)
-        w = power_iterations(R, w)
-        # attach the new eigenvector to W as a new column
-        W = np.c_[W, w]
-
-    # get eigenvalues and save them in the ev array
-    y = R0 * W
-    ev = LA.norm(y, axis=0) / LA.norm(W, axis=0)
-    return W, ev
-
-
-##############
-
-
-# digits = datasets.load_digits(n_class=6)
-# total_X = digits.data
-# total_y = digits.target
+digits = datasets.load_digits(n_class=6)
+total_X = digits.data
+total_y = digits.target
 
 # task 1: use first 100 data as base, loop through the following data to update covariance
 # matrix, then at last to compute eigenvectors
-# onlinePCA = OnlinePCA(X=total_X[:100])
-#
-# for i in range(101, total_X.shape[0]):
-#     onlinePCA.updateCov(total_X[i])
-#
-# W0, ev0 = onlinePCA.computePCA(n_pcs=2)
-#
-# prj = np.matrix(total_X) * W0[:, 0:2]
-# onlinePCA.plot_embedding(prj, total_y, "my onlinePCA result")
-# plt.show()
+
+onlinePCA = OnlinePCA(X=total_X[:100])
+
+for i in range(101, total_X.shape[0]):
+    onlinePCA.updateCov(total_X[i])
+
+W0, ev0 = onlinePCA.onlinePowerPCA(n_pcs=2)
+
+prj = np.matrix(total_X) * W0[:, 0:2]
+onlinePCA.plot_embedding(prj, total_y, "my onlinePCA result")
+plt.show()
 
 # compare the eigenvector gained in each loop with the original eigenvector obtained through
 # batch mode
 #
 # X = np.matrix(total_X)
-# m = np.mean(total_X, axis=0)
-# X -= m
-# R = X.T * X / (total_X.shape[0] - 1.0)
-# W0, ev0 = powerPCA(total_X, n_pcs=2)
+# print X.shape
+# W0, ev0 = OnlinePCA.powerPCA(X, n_pcs=2)
 # prj = X * W0[:, 0:2]
 #
 # w0 = W0.T[0]
