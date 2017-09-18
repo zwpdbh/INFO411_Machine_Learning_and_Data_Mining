6848b4cf13f7ab98dca9cd1f0161aaeefb413477
zwpdbh <hyperion_z@outlook.com>
2017-09-18 20:07:45 +1200

    <The very general G-means has been implemented>

--- assignment_2/GMeans.py
+++ assignment_2/GMeans.py
@@ -1,65 +1,73 @@
 import numpy as np
-
 import matplotlib.pyplot as plt
 
-from sklearn.cluster import MiniBatchKMeans
-from sklearn.preprocessing import scale
 from scipy.stats import anderson
 from KMeans import KMeans
 from Tools import Tools as tl
+from sklearn.preprocessing import scale
 
 class GMeans:
-    def __init__(self, dataSet):
-        self.X = dataSet.copy()
-        centroid = np.mean(self.X, axis=0)
-        self.centroids = np.array([centroid])
+    def __init__(self, strictLevel):
+        self.strickLevel = strictLevel
+        self.centroids = []
+        self.cluster = []
+
+    # recursive helper method
+    def computeGMeans(self, X):
+        centroid = np.mean(X, axis=0)
+        km = KMeans()
+        km.split(dataSet=X)
+        v = km.c_0 - km.c_1
+
+        X_prime = scale(X.dot(v) / (v.dot(v)))
+        accept_split = GMeans.checkGaussianStatistic(X_prime, self.strickLevel)
+
+        if accept_split:
+            self.computeGMeans(km.cluster_0)
+            self.computeGMeans(km.cluster_1)
+        else:
+            self.centroids.append(centroid)
 
     @staticmethod
-    def kmeans(dataSet, k):
-        kM = MiniBatchKMeans(n_clusters=k, init='k-means++')
-        kM.fit(X=dataSet)
-        return kM.cluster_centers_
+    def checkGaussianStatistic(X, strictLevel):
+        X = X - np.mean(X)
+        output = anderson(X)
+        statistic = output[0]
+        critical_value = output[1][strictLevel]
+        # print statistic, critical_value
+        if statistic >= critical_value:
+            return True
+        else:
+            return False
 
-    @staticmethod
-    def testGaussian(dataSet):
-        kM = MiniBatchKMeans(n_clusters=2, init='k-means++')
-        kM.fit(dataSet)
-        centroids = kM.cluster_centers_
-
-        c1 = centroids[0]
-        c2 = centroids[1]
-        v = c1 - c2
-        X_prime = scale(dataSet.dot(v)) / (v.dot(v))
-        GMeans.computeStatisticOfAnderson(X=X_prime)
-
-    @staticmethod
-    def plotCentroids(centroids):
-        plt.plot(centroids[:, 0], centroids[:, 1], 'ro')
-
-    @staticmethod
-    def computeStatisticOfAnderson(X):
-        print anderson(X)
+    def fit(self, X):
+        self.computeGMeans(X)
+        self.centroids = np.array(self.centroids)
 
 
 if __name__ == '__main__':
 
     dataSet1 = np.random.randn(100, 2)+2
     dataSet2 = np.random.randn(100, 2)+5
+    dataSet3 = np.random.randn(400, 2)-2
+    dataSet4 = np.random.randn(50, 2)-5
+    dataSet5 = np.random.randn(20, 2)-10
 
     total_dataSet = np.vstack((dataSet1, dataSet2))
+    total_dataSet = np.vstack((total_dataSet, dataSet3))
+    total_dataSet = np.vstack((total_dataSet, dataSet4))
+    total_dataSet = np.vstack((total_dataSet, dataSet5))
+
     total_dataSet = np.random.permutation(total_dataSet)
 
-    km = MiniBatchKMeans(2)
-    result = km.fit(total_dataSet)
-    print result.cluster_centers_
+    gM = GMeans(strictLevel=4)
+    gM.fit(total_dataSet)
 
-    km = KMeans()
-    km.split(dataSet=total_dataSet)
-    tl.drawCentroid(km.c_0, symbol='r^')
-    tl.drawDataSet(km.cluster_0, 'g+')
+    print "found {} clusters".format(len(gM.centroids))
 
-    tl.drawCentroid(km.c_1)
-    tl.drawDataSet(km.cluster_1, 'bx')
-
+    tl.drawDataSet(total_dataSet, 'g+')
+    tl.drawCentroids(gM.centroids)
     plt.show()
+
+
 
